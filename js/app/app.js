1e010ab5c2f24f11f8de4fdda4bcfa20ab324eb0
priyaelizabeth <priya.ollapally@nytimes.com>
2013-09-10 15:14:23 -0400

    fixing reference errors in url, and adding another article

--- js/app/app.js
+++ js/app/app.js
@@ -6,18 +6,19 @@ ChoiceyApp = {
 		refresh: $("#refresh-choices"),
 		oEls: [$("#option-1"), $("#option-2")],
 		pool: [
-			{ image: 'http://placehold.it/75x75', headline: 'TK National Zoo’s baby panda is female', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK One-Way Ticket to Mars Attracts Global Attention', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK \'Sixth sense\' exists in brain, scientist says', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK Tokyo wins bid to host 2020 Summer Olympics', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK Unrigging the Admissions System', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK Syria strike may hinge on Nancy Pelosi', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK World leaders push big companies to pay more taxes', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TK After NSA encryption-cracking revelation, can we trust Internet security?', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TKNew Mexico-size volcano discovered in the depths of the Pacific Ocean', link: 'article/article.html' },
-			{ image: 'http://placehold.it/75x75', headline: 'TKExperimental \'Armadillo\' car folds up for easy parking', link: 'experimental-armadillo-car-folds-easy-parking.html'}
+			{ image: 'http://i.huffpost.com/gen/1342502/thumbs/o-PANDA-CUB-900.jpg?6', headline: 'TK Meet the newest resident at the National Zoo', link: 'article/article.html' },
+			{ image: 'http://www.reuters.com/investigates/adoption/images/part1/ce1b.jpg', headline: 'TK Learn about the American underground market for adopted international children', link: 'article/article.html' },
+			{ image: 'http://newswatch.nationalgeographic.com/files/2013/09/Mars_one_16_1920x1080-600x337.jpg', headline: 'TK Consider the applicants for a one-way ticket to Mars', link: 'article/article.html' },
+			{ image: 'http://a57.foxnews.com/global.fncstatic.com/static/managed/img/Scitech/660/371/brain_power.jpg?ve=1', headline: 'TK Read about the discovery of the human brain\'s \'Sixth sense\'', link: 'article/article.html' },
+			{ image: 'http://media.npr.org///assets/img/2013/09/07/istock_000009357397small_wide-02f604b747809cb653bb6b4eaff2594b2dcfc3dc-s40-c85.jpg', headline: 'TK Hear about why you probably need to take a nap', link: 'article/memory-pinball-and-other-reasons.html' },
+			{ image: 'http://placehold.it/150x100', headline: 'TK After NSA encryption-cracking revelation, can we trust Internet security?', link: 'article/article.html' },
+			{ image: 'http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2013/09/06/Others/Images/2013-09-06/Tamu3D_v21378482912.jpg', headline: 'TK The largest known volcano, hiding quietly in the Pacific Ocean', link: 'article/article.html' },
+			{ image: 'http://timenewsfeed.files.wordpress.com/2013/09/fold-up-armadillo-car.jpg?w=480&h=320&crop=1', headline: 'TK Hear about the South Korean car nicknamed after a desert animal?', link: 'experimental-armadillo-car-folds-easy-parking.html'},
+			{ image: 'http://mjcdn.motherjones.com/preset_51/shampoo.jpg', headline: 'TK Learn about how every time you shampoo you\'re hurting the environment?', link: 'article/dont-lather-dont-rinse-dont-repeat.html' },
+			{ image: 'http://news.bbcimg.co.uk/media/images/69685000/jpg/_69685229_img_0578.jpg', headline: 'TK Meet the Arizona teens whose hobbies include performing public exorcisms?', link: 'article/teen-exorcists.html'}
+			
 		],
-		oString: '<img class="thumb" src="{0}"><h2><a href="{1}">{2}</a></h2>',
+		oString: '<a href="{1}"><img class="thumb" src="{0}"></a><h2><a href="{1}">{2}</a></h2>',
 		counter: 0,
 		maxOptions: 10
 	},
