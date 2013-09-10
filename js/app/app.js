1c7a4dab02af05ad43c89be0fda1b13de960a1d9
priyaelizabeth <priya.ollapally@nytimes.com>
2013-09-10 17:26:29 -0400

    keep track of options

--- js/app/app.js
+++ js/app/app.js
@@ -6,6 +6,8 @@ ChoiceyApp = {
 		refresh: $("#refresh-choices"),
 		oEls: [$("#option-1"), $("#option-2")],
 		pool: [
+			{ image: 'http://mjcdn.motherjones.com/preset_51/shampoo.jpg', headline: 'TK Learn about how every time you shampoo you\'re hurting the environment?', link: 'article/dont-lather-dont-rinse-dont-repeat.html' },
+			{ image: 'http://news.bbcimg.co.uk/media/images/69685000/jpg/_69685229_img_0578.jpg', headline: 'TK Meet the Arizona teens whose hobbies include performing public exorcisms?', link: 'article/teen-exorcists.html'},
 			{ image: 'http://i.huffpost.com/gen/1342502/thumbs/o-PANDA-CUB-900.jpg?6', headline: 'TK Meet the newest resident at the National Zoo', link: 'article/article.html' },
 			{ image: 'http://www.reuters.com/investigates/adoption/images/part1/ce1b.jpg', headline: 'TK Learn about the American underground market for adopted international children', link: 'article/article.html' },
 			{ image: 'http://newswatch.nationalgeographic.com/files/2013/09/Mars_one_16_1920x1080-600x337.jpg', headline: 'TK Consider the applicants for a one-way ticket to Mars', link: 'article/article.html' },
@@ -13,13 +15,10 @@ ChoiceyApp = {
 			{ image: 'http://media.npr.org///assets/img/2013/09/07/istock_000009357397small_wide-02f604b747809cb653bb6b4eaff2594b2dcfc3dc-s40-c85.jpg', headline: 'TK Hear about why you probably need to take a nap', link: 'article/memory-pinball-and-other-reasons.html' },
 			{ image: 'http://placehold.it/150x100', headline: 'TK After NSA encryption-cracking revelation, can we trust Internet security?', link: 'article/article.html' },
 			{ image: 'http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2013/09/06/Others/Images/2013-09-06/Tamu3D_v21378482912.jpg', headline: 'TK The largest known volcano, hiding quietly in the Pacific Ocean', link: 'article/article.html' },
-			{ image: 'http://timenewsfeed.files.wordpress.com/2013/09/fold-up-armadillo-car.jpg?w=480&h=320&crop=1', headline: 'TK Hear about the South Korean car nicknamed after a desert animal?', link: 'experimental-armadillo-car-folds-easy-parking.html'},
-			{ image: 'http://mjcdn.motherjones.com/preset_51/shampoo.jpg', headline: 'TK Learn about how every time you shampoo you\'re hurting the environment?', link: 'article/dont-lather-dont-rinse-dont-repeat.html' },
-			{ image: 'http://news.bbcimg.co.uk/media/images/69685000/jpg/_69685229_img_0578.jpg', headline: 'TK Meet the Arizona teens whose hobbies include performing public exorcisms?', link: 'article/teen-exorcists.html'}
-			
+			{ image: 'http://timenewsfeed.files.wordpress.com/2013/09/fold-up-armadillo-car.jpg?w=480&h=320&crop=1', headline: 'TK Hear about the South Korean car nicknamed after a desert animal?', link: 'experimental-armadillo-car-folds-easy-parking.html'}			
 		],
 		oString: '<a href="{1}"><img class="thumb" src="{0}"></a><h2><a href="{1}">{2}</a></h2>',
-		counter: 0,
+		counter: localStorage.getItem('option-counter') || 0,
 		maxOptions: 10
 	},
 
@@ -43,24 +42,18 @@ ChoiceyApp = {
 	
 	populateOptions: function() {
 		$.each(s.oEls, function(i,v){
-			v.fadeOut(50, function(){
-				v.html(ChoiceyApp.getNextStory()).fadeIn(50);
+			v.fadeOut(150, function(){
+				v.html(ChoiceyApp.getNextStory()).fadeIn(150);
 			});
 		});
 	},
-	
+
 	getNextStory: function() {
 		var str = s.oString.format(s.pool[s.counter].image, s.pool[s.counter].link, s.pool[s.counter].headline);
 		s.counter < s.maxOptions-1 ? s.counter++ : s.counter = 0;
+		localStorage.setItem('option-counter',s.counter);
 		return str;
-	},
-	
-	revealOptions: function() {
-		$.each(s.oEls, function(i,v){
-			v.animate({opacity: 1},100);
-		});
 	}
-	
 };
 
 (function() {
@@ -79,6 +72,6 @@ ChoiceyApp = {
 	
 	$(document).ready(function(){
 		ChoiceyApp.init();
-		ChoiceyApp.revealOptions();
 	})
+
 })();
